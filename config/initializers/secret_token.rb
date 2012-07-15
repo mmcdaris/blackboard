b2e7491dc74059e139246c7c2287ef52623decf9
Morgan Mikel McDaris <morganmcdaris@yahoo.com>
2012-07-15 13:18:06 -0700

    starting project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blackboard::Application.config.secret_token = '8dff823f27ebf705b04cbf93d0c813d02fb5edf699a168a9bff9e5563269389968f8fb2a284086a54c38d0593e7b51ef8502fa0394be4352911c44a2624e02e5'
